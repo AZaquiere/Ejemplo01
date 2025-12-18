e1433337fb94e42cb72d80f5e04ac43196cb5440
Agustín Zaquiere <agustinzaquiere@gmail.com>
2025-12-18 23:30:05 +0100

    Creado el ejemplo02

--- Ejemplo02/obj/Debug/net10.0/Ejemplo02.AssemblyInfo.cs
+++ Ejemplo02/obj/Debug/net10.0/Ejemplo02.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Ejemplo01")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Ejemplo02")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+529d4eb66c85654b105b6ad77944dcb5d196dedb")]
-[assembly: System.Reflection.AssemblyProductAttribute("Ejemplo01")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Ejemplo01")]
+[assembly: System.Reflection.AssemblyProductAttribute("Ejemplo02")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Ejemplo02")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
